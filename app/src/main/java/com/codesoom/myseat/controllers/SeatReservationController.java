eb690c2c8e757b6fab81bcbaa5976e7abe1b06a5
HanJiyoung <jiyounghaan@gmail.com>
2022-06-30 00:00:38 +0900

    좌석 예약 컨트롤러 생성

---
+++ app/src/main/java/com/codesoom/myseat/controllers/SeatReservationController.java
@@ -0,0 +1,18 @@
+package com.codesoom.myseat.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 좌석 예약 관련 요청 컨트롤러
+ */
+@RestController
+@RequestMapping("/seat")
+public class SeatReservationController {
+    // TODO: 좌석 예약
+    
+    // TODO: 좌석 예약 내역 조회
+
+    // TODO: 좌석 예약 수정
+    
+    // TODO: 좌석 예약 취소
+}
