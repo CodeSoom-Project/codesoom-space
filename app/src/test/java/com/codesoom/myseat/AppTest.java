f0f9ccc7905242c37738049a48a91719cbd1a983
HanJiyoung <jiyounghaan@gmail.com>
2022-06-29 16:52:13 +0900

    App의 불필요한 코드 제거 및 SpringBootApplication 설정

--- app/src/test/java/com/codesoom/myseat/AppTest.java
+++ app/src/test/java/com/codesoom/myseat/AppTest.java
@@ -1,11 +1,4 @@
 package com.codesoom.myseat;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
 }
