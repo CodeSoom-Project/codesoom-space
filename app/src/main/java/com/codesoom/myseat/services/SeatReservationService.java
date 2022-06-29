2083185cb0a4278df598dbea1b4db62bc0ddd3aa
HanJiyoung <jiyounghaan@gmail.com>
2022-06-30 00:00:49 +0900

    좌석 예약 서비스 생성

---
+++ app/src/main/java/com/codesoom/myseat/services/SeatReservationService.java
@@ -0,0 +1,17 @@
+package com.codesoom.myseat.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 좌석 예약 생성, 조회, 수정, 삭제 서비스
+ */
+@Service
+public class SeatReservationService {
+    // TODO: 좌석 예약 생성
+
+    // TODO: 좌석 예약 조회
+
+    // TODO: 좌석 예약 수정
+
+    // TODO: 좌석 예약 삭제
+}
