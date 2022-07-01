c12b948cb934ff9aca45dbd3c3889ae3ee280db9
HanJiyoung <jiyounghaan@gmail.com>
2022-07-01 15:48:14 +0900

    좌석 예약 서비스 메서드 내부 로직 분리
    기존에 addReservation() 내에서 좌석 조회, 예약 여부 확인, 현재 날짜 구하기를 모두 처리하도록 했었는데, 이를 각각의 메서드로 분리했습니다.

--- app/src/main/java/com/codesoom/myseat/exceptions/SeatAlreadyReservedException.java
+++ app/src/main/java/com/codesoom/myseat/exceptions/SeatAlreadyReservedException.java
@@ -3,8 +3,8 @@ package com.codesoom.myseat.exceptions;
 /**
  * 좌석이 이미 사용중일 때 발생하는 예외
  */
-public class SeatAlreadyInUseException extends RuntimeException {
-    public SeatAlreadyInUseException(String message) {
+public class SeatAlreadyReservedException extends RuntimeException {
+    public SeatAlreadyReservedException(String message) {
         super(message);
     }
 }
--- app/src/main/java/com/codesoom/myseat/services/SeatReservationService.java
+++ app/src/main/java/com/codesoom/myseat/services/SeatReservationService.java
@@ -5,7 +5,7 @@ import com.codesoom.myseat.domain.SeatRepository;
 import com.codesoom.myseat.domain.SeatReservation;
 import com.codesoom.myseat.domain.SeatReservationRepository;
 import com.codesoom.myseat.dto.SeatReservationRequest;
-import com.codesoom.myseat.exceptions.SeatAlreadyInUseException;
+import com.codesoom.myseat.exceptions.SeatAlreadyReservedException;
 import com.codesoom.myseat.exceptions.SeatNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -39,20 +39,13 @@ public class SeatReservationService {
             int number,
             SeatReservationRequest seatReservationRequest
     ) {
-        Seat seat = findSeat(number);
-        if(seat.isReserved() == true) {
-            throw new SeatAlreadyInUseException(
-                    "[" + number + "]번 좌석이 이미 사용중이어서 예약에 실패했습니다.");
-        }
-
-        LocalDateTime now = LocalDateTime.now();
-        String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        checkReservationStatus(number);
 
         return seatReservationRepository.save(
                 SeatReservation.builder()
                         .seatNumber(number)
                         .userName(seatReservationRequest.getUserName())
-                        .date(date)
+                        .date(getDate())
                         .checkIn(seatReservationRequest.getCheckIn())
                         .checkOut(seatReservationRequest.getCheckOut())
                         .build());
@@ -74,5 +67,31 @@ public class SeatReservationService {
         return seatRepository.findByNumber(number)
                 .orElseThrow(() -> new SeatNotFoundException(
                         "[" + number + "]번 좌석을 찾을 수 없어서 조회에 실패했습니다."));
+    }
+
+    /**
+     * 좌석이 이미 예약된 상태인지 확인한다.
+     *
+     * @param number 좌석 번호
+     * @throws SeatNotFoundException 좌석을 찾을 수 없는 경우 예외를 던진다.
+     * @throws SeatAlreadyReservedException 좌석이 이미 예약된 상태일 경우 예외를 던진다.
+     */
+    private void checkReservationStatus(int number) {
+        Seat seat = findSeat(number);
+
+        if(seat.isReserved()) {
+            throw new SeatAlreadyReservedException(
+                    "[" + number + "]번 좌석은 이미 예약된 좌석이므로 예약에 실패했습니다.");
+        }
+    }
+
+    /**
+     * 오늘 날짜를 반환한다.
+     *
+     * @return 오늘 날짜
+     */
+    private String getDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
