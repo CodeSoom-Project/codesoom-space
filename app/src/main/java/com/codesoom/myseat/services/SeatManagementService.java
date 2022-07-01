7623339d9d969ebefa9139ddbfa2d9f333e8a395
HanJiyoung <jiyounghaan@gmail.com>
2022-07-01 14:44:04 +0900

    좌석 관리 서비스 생성

---
+++ app/src/main/java/com/codesoom/myseat/services/SeatManagementService.java
@@ -0,0 +1,17 @@
+package com.codesoom.myseat.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 좌석 관리 서비스
+ */
+@Service
+public class SeatManagementService {
+    // TODO: 좌석 추가
+    
+    // TODO: 좌석 목록 조회
+    
+    // TODO: 좌석 수정
+    
+    // TODO: 좌석 삭제
+}
