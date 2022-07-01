0ccc09d643cc007209aa1f36cd7dc208b043a9a1
HanJiyoung <jiyounghaan@gmail.com>
2022-07-01 14:42:49 +0900

    좌석 관리 컨트롤러 생성

---
+++ app/src/main/java/com/codesoom/myseat/controllers/SeatManagementController.java
@@ -0,0 +1,19 @@
+package com.codesoom.myseat.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 좌석 관리 요청 컨트롤러
+ */
+@RestController
+@RequestMapping("/seat")
+public class SeatManagementController {
+    // TODO: 좌석 추가
+
+    // TODO: 좌석 목록 조회
+
+    // TODO: 좌석 수정
+
+    // TODO: 좌석 삭제
+}
