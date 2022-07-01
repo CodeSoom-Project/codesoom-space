25b9caeacefe6d66aadd79f23a69e5a8744b99d3
HanJiyoung <jiyounghaan@gmail.com>
2022-07-01 17:34:14 +0900

    ErrorAdvice 추가
    컨트롤러에서 예외 발생 시 에러 메시지를 응답하는 ErrorAdvice를 추가했습니다.

---
+++ app/src/main/java/com/codesoom/myseat/controllers/ControllerErrorAdvice.java
@@ -0,0 +1,39 @@
+package com.codesoom.myseat.controllers;
+
+import com.codesoom.myseat.dto.ErrorResponse;
+import com.codesoom.myseat.exceptions.SeatAlreadyReservedException;
+import com.codesoom.myseat.exceptions.SeatNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 컨트롤러에서 발생하는 예외를 처리한다.
+ */
+@ResponseBody
+@ControllerAdvice
+public class ControllerErrorAdvice {
+    /**
+     * 좌석을 찾지 못했을 때 에러메시지를 반환한다.
+     *
+     * @return 에러메시지
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(SeatNotFoundException.class)
+    public ErrorResponse handleSeatNotFound() {
+        return new ErrorResponse("좌석을 찾을 수 없습니다.");
+    }
+
+    /**
+     * 이미 예약된 좌석일 때 에러메시지를 반환한다.
+     * 
+     * @return 에러메시지
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(SeatAlreadyReservedException.class)
+    public ErrorResponse handleSeatAlreadyReserved() {
+        return new ErrorResponse("이미 예약된 좌석입니다.");
+    }
+}
