25c4d21a89ea068a1e8e1ec3b9e7f7f1d03c3f8a
HanJiyoung <jiyounghaan@gmail.com>
2022-07-05 02:22:10 +0900

    좌석 예약 컨트롤러에 CrossOrigin 어노테이션 추가
    프론트엔드 서버는 백엔드 서버와 다른 포트를 사용하기 때문에, 두 포트 간 자원 공유가 가능하도록 CORS 처리를 했습니다.

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/seat")
+@CrossOrigin
 public class SeatReservationController {
     private final SeatReservationService seatReservationService;
 
