34f7c44f5e37bdf1f45617cc8455775f09364284
HanJiyoung <jiyounghaan@gmail.com>
2022-07-07 20:31:03 +0900

    좌석 예약 목록 조회 컨트롤러 메서드 구현

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationListController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationListController.java
@@ -1,10 +1,12 @@
 package com.codesoom.myseat.controllers;
 
+import com.codesoom.myseat.domain.SeatReservation;
 import com.codesoom.myseat.dto.SeatReservationResponse;
 import com.codesoom.myseat.services.SeatReservationListService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,9 +22,39 @@ public class SeatReservationListController {
         this.service = service;
     }
 
+    /**
+     * 상태코드 200과 좌석 예약 목록을 응답한다.
+     * 
+     * @return 좌석 예약 목록
+     */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<SeatReservationResponse> seatReservations() {
-        return null;
+        return toResponse(service.seatReservations());
+    }
+
+    /**
+     * 좌석 예약 목록 DTO를 반환한다.
+     *
+     * @param data 좌석 예약 목록
+     * @return 좌석 예약 목록 DTO
+     */
+    private List<SeatReservationResponse> toResponse(List<SeatReservation> data) {
+        List<SeatReservationResponse> list = new ArrayList<>();
+        SeatReservationResponse response;
+
+        for(SeatReservation s : data) {
+            response = SeatReservationResponse.builder()
+                    .userName(s.getUserName())
+                    .seatNumber(s.getSeatNumber())
+                    .date(s.getDate())
+                    .checkIn(s.getCheckIn())
+                    .checkOut(s.getCheckOut())
+                    .build();
+
+            list.add(response);
+        }
+
+        return list;
     }
 }
