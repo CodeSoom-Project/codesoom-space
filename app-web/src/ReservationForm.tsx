208dda384ce246dd7cba47ab4a21fa226eb72c04
0hu <dladudgn123@naver.com>
2022-07-08 17:05:37 +0900

    form componet 분리

--- app-web/src/ReservationForm.tsx
+++ app-web/src/ReservationForm.tsx
@@ -1,14 +1,42 @@
-import { useFrom } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 function ReservationForm() {
-    const { register } = useFrom();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors }
+    } = useForm();
+
+    console.log(errors);
+
     return (
         <section>
-          <form>
-            <input {...register("seatNumber")} placeholder="좌석 번호" />
-            <input {...register("name")} placeholder="이름" />
-            <input {...register("checkIn")} placeholder="예약 시작 시간(hh:mm)" />
-            <input {...register("name")} placeholder="이용 종료 시간(hh:mm)" />
+          <form
+            onSubmit={handleSubmit((data) => {
+                console.log(data);
+              })}
+            >
+            <input
+              {...register("seatNumber", { required: '좌석 번호를 입력해 주세요'})}
+              placeholder="좌석 번호"
+            />
+            <input
+              {...register("name",{ required: '이름을 입력해 주세요'})}
+              placeholder="이름"
+            />
+            <input
+              {...register("checkIn",{ required: '예약 시작 시간을 입력해 주세요'})}
+              placeholder="예약 시작 시간(hh:mm)"
+            />
+            <input
+              {...register("name",{ required: '이용 종료 시간을 입력해 주세요'})}
+              placeholder="이용 종료 시간(hh:mm)"
+            />
+            <button
+              type="button"
+            >
+              예약하기
+            </button>
           </form>
         </section>
     );
--- app-web/src/ReservationFormContainer.tsx
+++ app-web/src/ReservationFormContainer.tsx
@@ -3,51 +3,51 @@ import ReservationForm from "./ReservationForm";
 
 function ReservationFormContainer() {
 
-    const onChangeSeatNumber = (e:any) => {
-        e.preventDefault();
-        setSeatNumber(e.target.value);
-    };
-    const onChangeUserName = (e:any) => {
-        e.preventDefault();
-        setUserName(e.target.value);
-    };
-    const onChangeCheckIn = (e:any) => {
-        e.preventDefault();
-        setCheckIn(e.target.value);
-    };
-    const onChangeCheckOut = (e:any) => {
-        e.preventDefault();
-        setCheckOut(e.target.value);
-    };
-
-    const handleSubmit = async(e:any) => {
-        e.preventDefault();
-
-        const body = {
-            name: userName,
-            checkIn: checkIn,
-            checkOut: checkOut
-        };
-
-        await axios.post('http://localhost:8080/seat/' + seatNumber,  {
-            body,
-        })
-            .then((response) => {
-                if(response.status === 201) {
-                    alert('예약 완료');
-                }
-            })
-            .catch((error) => {
-                console.log(error.response);
-
-                if(error.response.status === 400) {
-                    alert('이미 예약된 좌석입니다');
-                } else if(error.response.status === 404) {
-                    alert('존재하지 않는 좌석입니다');
-                }
-            });
-
-    }
+    // const onChangeSeatNumber = (e:any) => {
+    //     e.preventDefault();
+    //     setSeatNumber(e.target.value);
+    // };
+    // const onChangeUserName = (e:any) => {
+    //     e.preventDefault();
+    //     setUserName(e.target.value);
+    // };
+    // const onChangeCheckIn = (e:any) => {
+    //     e.preventDefault();
+    //     setCheckIn(e.target.value);
+    // };
+    // const onChangeCheckOut = (e:any) => {
+    //     e.preventDefault();
+    //     setCheckOut(e.target.value);
+    // };
+    //
+    // const handleSubmit = async(e:any) => {
+    //     e.preventDefault();
+    //
+    //     const body = {
+    //         name: userName,
+    //         checkIn: checkIn,
+    //         checkOut: checkOut
+    //     };
+    //
+    //     await axios.post('http://localhost:8080/seat/' + seatNumber,  {
+    //         body,
+    //     })
+    //         .then((response) => {
+    //             if(response.status === 201) {
+    //                 alert('예약 완료');
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             console.log(error.response);
+    //
+    //             if(error.response.status === 400) {
+    //                 alert('이미 예약된 좌석입니다');
+    //             } else if(error.response.status === 404) {
+    //                 alert('존재하지 않는 좌석입니다');
+    //             }
+    //         });
+    //
+    // }
 
     return (
         <ReservationForm />
