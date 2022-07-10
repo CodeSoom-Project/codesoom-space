f13b92b8b7d97b9dc5aeeafe7663d897bbf2a121
HanJiyoung <jiyounghaan@gmail.com>
2022-07-10 23:19:02 +0900

    좌석 예약 취소 컨트롤러 생성

---
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationCancelController.java
@@ -0,0 +1,14 @@
+package com.codesoom.myseat.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 좌석 예약 취소 컨트롤러
+ */
+@RestController
+@RequestMapping("/seat-reservation")
+@CrossOrigin
+public class SeatReservationCancelController {
+}
