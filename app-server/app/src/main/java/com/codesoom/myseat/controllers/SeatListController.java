5877e7323ccba5a04bed9b6cff7fb738b7f98b6b
HanJiyoung <jiyounghaan@gmail.com>
2022-07-14 15:32:01 +0900

    좌석 목록 조회 컨트롤러 생성

---
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatListController.java
@@ -0,0 +1,14 @@
+package com.codesoom.myseat.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 좌석 목록 조회 컨트롤러
+ */
+@RestController
+@RequestMapping("/seats")
+@CrossOrigin
+public class SeatListController {
+}
