3ef03f3168b9860fb3ed1e58cf430a42638c9635
HanJiyoung <jiyounghaan@gmail.com>
2022-07-14 16:56:14 +0900

    좌석 목록 조회 요청에 대한 happy path 테스트 작성

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatListController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatListController.java
@@ -1,8 +1,12 @@
 package com.codesoom.myseat.controllers;
 
+import com.codesoom.myseat.dto.SeatListResponse;
+import com.codesoom.myseat.services.SeatListService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 좌석 목록 조회 컨트롤러
@@ -11,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/seats")
 @CrossOrigin
 public class SeatListController {
+    private final SeatListService service;
+
+    public SeatListController(SeatListService service) {
+        this.service = service;
+    }
+
+    public List<SeatListResponse> seats() {
+        return null;
+    }
 }
--- app-server/app/src/main/java/com/codesoom/myseat/services/SeatListService.java
+++ app-server/app/src/main/java/com/codesoom/myseat/services/SeatListService.java
@@ -1,10 +1,16 @@
 package com.codesoom.myseat.services;
 
+import com.codesoom.myseat.domain.Seat;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 좌석 목록 조회 서비스
  */
 @Service
 public class SeatListService {
+    public List<Seat> seats() {
+        return null;
+    }
 }
