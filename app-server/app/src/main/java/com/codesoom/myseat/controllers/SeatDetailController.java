e48c1430f2dd8492784d1abfdcf72b9193d622b2
HanJiyoung <jiyounghaan@gmail.com>
2022-07-18 16:51:02 +0900

    좌석 상세 조회 컨트롤러 구현

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatDetailController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatDetailController.java
@@ -1,5 +1,6 @@
 package com.codesoom.myseat.controllers;
 
+import com.codesoom.myseat.domain.SeatReservation;
 import com.codesoom.myseat.dto.SeatReservationResponse;
 import com.codesoom.myseat.services.SeatDetailService;
 import org.springframework.http.HttpStatus;
@@ -17,9 +18,31 @@ public class SeatDetailController {
         this.service = service;
     }
 
-    @GetMapping
+    /**
+     * 좌석 상세 조회 후 상태코드 200을 응답한다.
+     *
+     * @param seatNumber 조회할 좌석 번호
+     * @return 좌석 예약 정보
+     */
+    @GetMapping("{seatNumber}")
     @ResponseStatus(HttpStatus.OK)
     public SeatReservationResponse seatDetail(@PathVariable int seatNumber) {
-        return null;
+        return toResponse(service.seatDetail(seatNumber));
+    }
+
+    /**
+     * 응답 정보를 반환한다.
+     *
+     * @param data 좌석 예약 정보
+     * @return 응답 정보
+     */
+    private SeatReservationResponse toResponse(SeatReservation data) {
+        return SeatReservationResponse.builder()
+                .userName(data.getUserName())
+                .seatNumber(data.getSeatNumber())
+                .date(data.getDate())
+                .checkIn(data.getCheckIn())
+                .checkOut(data.getCheckOut())
+                .build();
     }
 }
