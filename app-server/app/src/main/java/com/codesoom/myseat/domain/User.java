5d878d0b44f753719943381845ea1463d680738d
HanJiyoung <jiyounghaan@gmail.com>
2022-07-26 17:19:08 +0900

    회원 엔티티 생성

---
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/User.java
@@ -0,0 +1,30 @@
+package com.codesoom.myseat.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 회원 엔티티
+ */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    
+    private String email;
+    
+    private String password;
+}
