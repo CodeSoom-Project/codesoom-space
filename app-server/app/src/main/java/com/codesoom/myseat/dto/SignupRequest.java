4c5b9bd9080b15bdbaba72fa209df14605c7e029
HanJiyoung <jiyounghaan@gmail.com>
2022-07-26 17:18:57 +0900

    회원 가입 요청 DTO 생성

---
+++ app-server/app/src/main/java/com/codesoom/myseat/dto/SignupRequest.java
@@ -0,0 +1,19 @@
+package com.codesoom.myseat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원 가입 요청 정보
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    private String name;
+    private String email;
+    private String password;
+}
