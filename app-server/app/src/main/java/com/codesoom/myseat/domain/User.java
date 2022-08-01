9ee175d0f348dc5e97a767b25fc2d6188ee58bdc
HanJiyoung <jiyounghaan@gmail.com>
2022-08-01 13:04:49 +0900

    비밀번호 인증 로직 추가

--- app-server/app/src/main/java/com/codesoom/myseat/domain/User.java
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/User.java
@@ -27,4 +27,14 @@ public class User {
     private String email;
     
     private String password;
+
+    /**
+     * 비밀번호를 인증한다.
+     * 
+     * @param password 인증할 비밀번호
+     * @return 엔티티의 비밀번호와 일치하면 true, 일치하지 않으면 false
+     */
+    public boolean authenticate(String password) {
+        return password == this.password;
+    }
 }
