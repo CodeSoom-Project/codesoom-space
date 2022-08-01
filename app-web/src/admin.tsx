b840f4f41cee4a0d12fb6dee6ed61a53adc1a765
0hu <dladudgn123@naver.com>
2022-08-01 20:15:22 +0900

    admin page가 필요합니다
    admin page를 만들었습니다.

---
+++ app-web/src/admin.tsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+const Admin = () => {
+  return (
+    <section>
+      <div>
+        <Link to='/'>home</Link>
+      </div>
+      <h1>관리자 페이지</h1>
+      <br/>
+
+    </section>
+  );
+};
+
+export default Admin;
