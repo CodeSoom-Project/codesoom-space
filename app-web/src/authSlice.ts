dae98781b9851215529a49d984a263b062390837
0hu <dladudgn123@naver.com>
2022-08-03 12:40:31 +0900

    accessToken slice 수정이 필요해 보입니다.
    ...state 로 변경, null 이 아니라 '' 빈값으로 accessToken 기본값 변경하였습니다.

--- app-web/src/authSlice.ts
+++ app-web/src/authSlice.ts
@@ -2,13 +2,20 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {accessToken: null},
+  initialState: {accessToken: ''},
   reducers: {
-    setAccessToken: (state, action) => {
-      state.accessToken = action.payload
+    setAccessToken(state, {payload: accessToken}) {
+      return {
+        ...state,
+        accessToken,
+      }
     },
-    logout: (state, action) => {
-      state.accessToken = null
+
+    logout(state) {
+      return {
+        ...state,
+        accessToken: '',
+      }
     }
   },
 })
