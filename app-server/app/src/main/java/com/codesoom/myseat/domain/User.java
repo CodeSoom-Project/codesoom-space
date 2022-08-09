0ccb70d567db77fff6a792e1f41f1fa8793c4cd6
HanJiyoung <jiyounghaan@gmail.com>
2022-08-09 13:40:54 +0900

    비밀번호를 equals로 비교하도록 수정
    기존에 두 비밀번호가 일치하는지 확인하기 위해 == 비교를 했었는데, 이는 객체를 비교하는 것이기 때문에 문자열 비교에는 적합하지 않습니다. 따라서 equals로 수정했습니다.

--- app-server/app/src/main/java/com/codesoom/myseat/domain/User.java
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/User.java
@@ -35,6 +35,6 @@ public class User {
      * @return 엔티티의 비밀번호와 일치하면 true, 일치하지 않으면 false
      */
     public boolean authenticate(String password) {
-        return password == this.password;
+        return this.password.equals(password); 
     }
 }
