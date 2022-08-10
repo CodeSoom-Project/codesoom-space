2afcd376b7ed036ccbe973c6864b2cc0ee205b0c
0hu <dladudgn123@naver.com>
2022-08-10 10:13:06 +0900

    eslint 설정을 하라
    설정이 적용되지않아 다시 적용하였습니다.

--- app-web/.eslintrc.cjs
+++ app-web/.eslintrc.cjs
@@ -1,29 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        // suppress errors for missing 'import React' in files
-        "react/react-in-jsx-scope": "off",
-        // allow jsx syntax in js files (for next.js project)
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx",".ts",".tsx"] }], //should add ".ts" if typescript project
-    }
-}
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript',
+    'plugin:import/recommended'
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  ignorePatterns: ['.eslintrc.cjs'],
+  "rules": {
+    "react/jsx-filename-extension": [0],
+    "import/extensions": "off"
+  }
+};
