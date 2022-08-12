9124e69ec68efa164b169df6d9a10b93066847cd
HanJiyoung <jiyounghaan@gmail.com>
2022-08-12 16:43:04 +0900

    매일 23시에 좌석 예약 여부를 초기화하는 스케줄러 추가

--- app-server/app/src/main/java/com/codesoom/myseat/domain/SeatRepository.java
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/SeatRepository.java
@@ -1,12 +1,15 @@
 package com.codesoom.myseat.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface SeatRepository
-        extends CrudRepository<Seat, Long> {
+        extends JpaRepository<Seat, Long> {
     /**
      * 조회된 좌석을 반환한다.
      *
@@ -29,4 +32,9 @@ public interface SeatRepository
      * @return 좌석 목록
      */
     List<Seat> findAll();
+
+    @Query("UPDATE Seat SET isReserved=false WHERE isReserved is true")
+    @Modifying
+    @Transactional
+    void seatReservationReset();
 }
---
+++ app-server/app/src/main/java/com/codesoom/myseat/utils/ScheduledTasks.java
@@ -0,0 +1,34 @@
+package com.codesoom.myseat.utils;
+
+import com.codesoom.myseat.domain.SeatRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 스케줄링이 필요한 작업들을 관리하는 클래스
+ */
+@Component
+@Slf4j
+public class ScheduledTasks {
+    private static final SimpleDateFormat dateFormat 
+            = new SimpleDateFormat("HH:mm:ss");
+    
+    private final SeatRepository repository;
+
+    public ScheduledTasks(SeatRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * 매일 23시에 좌석 예약 여부를 초기화한다.
+     */
+    @Scheduled(cron = "0 0 23 * * *")
+    public void reportCurrentTime() {
+        log.info("현재 시간: {}", dateFormat.format(new Date()));
+        repository.seatReservationReset();
+    }
+}
