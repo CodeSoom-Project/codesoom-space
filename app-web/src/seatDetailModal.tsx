3078808f1b9f4ddad640db2232b0734bfb1dc1a1
0hu <dladudgn123@naver.com>
2022-08-18 22:07:14 +0900

    SeatDetail을 조건에 맞추어 보여주어라
    seatDetail에 맞추어 다르게 보여주어라

--- app-web/src/seatDetailModal.tsx
+++ app-web/src/seatDetailModal.tsx
@@ -1,19 +1,44 @@
 import Modal from './components/Modal';
 import Detail from './components/details';
-import ReservationButton from './ReservationButton';
-import SelectCheckTime from './SelectCheckTime';
+import SelectTimeContainer from './SelectTimeContainer';
 
 
-export default function SeatDetailModal({ open, onClose, seatNumber, seatDetail, onClick, onDelete, onChangeCheckIn, onChangeCheckOut }:any) {
-  const { userName, , date, checkIn, checkOut } = seatDetail;
+export default function SeatDetailModal({ open, onClose, seatNumber, seatDetail, onClick, onDelete, isMySeat }:any) {
+  if (!seatDetail) return (
+    <>
+      <Modal open={open} onClose={onClose}>
+        <Detail
+          userName=''
+          seatNumber={seatNumber}
+          checkIn=''
+          checkOut=''
+        />
+        <SelectTimeContainer
+          seatNumber={seatNumber}
+          onClick={onClick}
+          onDelete={onDelete}
+          isMySeat={isMySeat}
+        />
+      </Modal>
+    </>
+  );
+  const { userName, seatDetailNumber, date, checkIn, checkOut } = seatDetail;
 
   return (
     <>
-      <Modal open={open} onclose={onClose}>
-        <Detail userName={userName} seatNumber={seatNumber} checkIn={checkIn} checkOut={checkOut} />
-        <SelectCheckTime children={'예약 시작 시간'} onChangeCheckIn={onChangeCheckIn} />
-        <SelectCheckTime children={'예약 종료 시간'} onChangeCheckOut={onChangeCheckOut} />
-        {/*<ReservationButton onClick={onClick} onDelete={onDelete} isMySeat={isMySeat} />*/}
+      <Modal open={open} onClose={onClose}>
+        <Detail
+          userName={userName}
+          seatNumber={seatNumber}
+          checkIn={checkIn}
+          checkOut={checkOut}
+        />
+        <SelectTimeContainer
+          seatNumber={seatNumber}
+          onClick={onClick}
+          onDelete={onDelete}
+          isMySeat={isMySeat}
+        />
       </Modal>
     </>
   );
