58353659fc3984f0bb9db9a7d92683098894c716
HanJiyoung <jiyounghaan@gmail.com>
2022-08-22 17:41:06 +0900

    좌석 상세 조회 응답 DTO 추가

---
+++ app-server/app/src/main/java/com/codesoom/myseat/dto/SeatDetailResponse.java
@@ -0,0 +1,21 @@
+package com.codesoom.myseat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 좌석 상세 조회 응답 정보
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class SeatDetailResponse {
+    private int seatNumber;
+    private String date;
+    private String checkIn;
+    private String checkOut;
+    private String userName;
+    private boolean isReserved;
+    private boolean isMySeat;
+}
