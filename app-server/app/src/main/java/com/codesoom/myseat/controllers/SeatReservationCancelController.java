34497ba911f58cb97929fbdf12cfa0d6feb758ad
HanJiyoung <jiyounghaan@gmail.com>
2022-09-26 17:32:09 +0900

    주석처리

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationCancelController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/SeatReservationCancelController.java
@@ -1,39 +1,40 @@
-package com.codesoom.myseat.controllers;
-
-import com.codesoom.myseat.domain.User;
-import com.codesoom.myseat.services.SeatReservationCancelService;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-/**
- * 좌석 예약 취소 컨트롤러
- */
-@RestController
-@RequestMapping("/seat-reservation")
-@CrossOrigin(
-//        origins = "https://codesoom-project.github.io",
-        origins = "*",
-        allowedHeaders = "*",
-        allowCredentials = "true")
-public class SeatReservationCancelController {
-    private final SeatReservationCancelService service;
-
-    public SeatReservationCancelController(SeatReservationCancelService service) {
-        this.service = service;
-    }
-
-    /**
-     * 좌석 예약을 취소한 후 상태코드 204를 응답한다.
-     *
-     * @param seatNumber 예약 취소할 좌석 번호
-     */
-    @DeleteMapping("{seatNumber}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancelReservation(
-            @PathVariable int seatNumber,
-            @AuthenticationPrincipal User user
-    ) {
-        service.cancelReservation(seatNumber, user);
-    }
-}
+//package com.codesoom.myseat.controllers;
+//
+//import com.codesoom.myseat.domain.User;
+//import com.codesoom.myseat.services.SeatReservationCancelService;
+//import org.springframework.http.HttpStatus;
+////import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.web.bind.annotation.*;
+//
+///**
+// * 좌석 예약 취소 컨트롤러
+// */
+//@RestController
+//@RequestMapping("/seat-reservation")
+//@CrossOrigin(
+////        origins = "https://codesoom-project.github.io",
+//        origins = "*",
+//        allowedHeaders = "*",
+//        allowCredentials = "true")
+//public class SeatReservationCancelController {
+//    private final SeatReservationCancelService service;
+//
+//    public SeatReservationCancelController(SeatReservationCancelService service) {
+//        this.service = service;
+//    }
+//
+//    /**
+//     * 좌석 예약을 취소한 후 상태코드 204를 응답한다.
+//     *
+//     * @param seatNumber 예약 취소할 좌석 번호
+//     */
+//    @DeleteMapping("{seatNumber}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void cancelReservation(
+//            @PathVariable int seatNumber
+////            @AuthenticationPrincipal User user
+//    ) {
+//        service.cancelReservation(seatNumber);
+////        service.cancelReservation(seatNumber, user);
+//    }
+//}
--- app-server/app/src/test/java/com/codesoom/myseat/controllers/SeatReservationControllerTest.java
+++ app-server/app/src/test/java/com/codesoom/myseat/controllers/SeatReservationControllerTest.java
@@ -1,113 +1,122 @@
-//package com.codesoom.myseat.controllers;
-//
-//import com.codesoom.myseat.domain.SeatReservation;
-//import com.codesoom.myseat.dto.SeatReservationRequest;
-//import com.codesoom.myseat.services.SeatReservationService;
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.restdocs.payload.JsonFieldType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-//import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-//import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-//import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-//import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-//import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-//import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-//import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@WebMvcTest(SeatReservationController.class)
-//@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.codesoom-myseat.site")
-//class SeatReservationControllerTest {
-//    private static final int SEAT_NUMBER = 3;
-//    private static final String USER_NAME = "코드숨";
-//    private static final Long SEAT_RESERVATION_ID = 1L;
-//    private static final String DATE = "2022-06-30";
-//    private static final String CHECK_IN = "09:30";
-//    private static final String CHECK_OUT = "17:30";
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private SeatReservationService service;
-//
-//    private SeatReservationRequest request;
-//    private SeatReservation reservation;
-//
-//    @Test
-//    @DisplayName("좌석 예약 요청 테스트")
-//    void test() throws Exception {
-//        // given
-//        request = SeatReservationRequest.builder()
+package com.codesoom.myseat.controllers;
+
+import com.codesoom.myseat.domain.SeatReservation;
+import com.codesoom.myseat.dto.SeatReservationRequest;
+import com.codesoom.myseat.services.SeatReservationService;
+import com.codesoom.myseat.utils.JwtUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SeatReservationController.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.codesoom-myseat.site")
+class SeatReservationControllerTest {
+    private static final int SEAT_NUMBER = 3;
+    private static final String USER_NAME = "코드숨";
+    private static final String USER_EMAIL = "test@email.com";
+    private static final Long SEAT_RESERVATION_ID = 1L;
+    private static final String DATE = "2022-06-30";
+    private static final String CHECK_IN = "09:30";
+    private static final String CHECK_OUT = "17:30";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SeatReservationService service;
+
+    private SeatReservationRequest request;
+    private SeatReservation reservation;
+    
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Test
+    @DisplayName("좌석 예약 요청 테스트")
+    void test() throws Exception {
+        String token = jwtUtil.makeJwtToken(USER_EMAIL);
+        
+        // given
+        request = SeatReservationRequest.builder()
 //                .userName(USER_NAME)
-//                .checkIn(CHECK_IN)
-//                .checkOut(CHECK_OUT)
-//                .build();
-//
-//        reservation = SeatReservation.builder()
-//                .id(SEAT_RESERVATION_ID)
+                .checkIn(CHECK_IN)
+                .checkOut(CHECK_OUT)
+                .build();
+
+        reservation = SeatReservation.builder()
+                .id(SEAT_RESERVATION_ID)
 //                .seatNumber(SEAT_NUMBER)
 //                .userName(USER_NAME)
-//                .date(DATE)
-//                .checkIn(CHECK_IN)
-//                .checkOut(CHECK_OUT)
-//                .build();
-//
-//        given(service.addReservation(eq(SEAT_NUMBER), any(SeatReservationRequest.class)))
-//                .willReturn(reservation);
-//
-//        // when
-//        ResultActions subject = mockMvc.perform(post("/seat-reservation/{seatNumber}", SEAT_NUMBER)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(toJson(request)));
-//
-//        // then
-//        subject.andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.userName").value(USER_NAME))
-//                .andExpect(jsonPath("$.seatNumber").value(SEAT_NUMBER))
-//                .andExpect(jsonPath("$.date").value(DATE))
-//                .andExpect(jsonPath("$.checkIn").value(CHECK_IN))
-//                .andExpect(jsonPath("$.checkOut").value(CHECK_OUT));
-//
-//        // docs
-//        subject.andDo(document("seat-reservation",
-//                preprocessRequest(prettyPrint()),
-//                preprocessResponse(prettyPrint()),
-//                pathParameters(
-//                        parameterWithName("seatNumber").description("좌석 번호")
-//                ),
-//                requestFields(
-//                        fieldWithPath("userName").type(JsonFieldType.STRING).description("회원 이름"),
-//                        fieldWithPath("checkIn").type(JsonFieldType.STRING).description("체크인 시간"),
-//                        fieldWithPath("checkOut").type(JsonFieldType.STRING).description("체크아웃 시간")
-//                ),
-//                responseFields(
-//                        fieldWithPath("userName").type(JsonFieldType.STRING).description("회원 이름"),
-//                        fieldWithPath("seatNumber").type(JsonFieldType.NUMBER).description("좌석 번호"),
-//                        fieldWithPath("date").type(JsonFieldType.STRING).description("예약 날짜"),
-//                        fieldWithPath("checkIn").type(JsonFieldType.STRING).description("체크인 시간"),
-//                        fieldWithPath("checkOut").type(JsonFieldType.STRING).description("체크아웃 시간")
-//                )
-//        ));
-//    }
-//
-//    private String toJson(Object object) throws JsonProcessingException {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        return objectMapper.writeValueAsString(object);
-//    }
-//}
+                .date(DATE)
+                .checkIn(CHECK_IN)
+                .checkOut(CHECK_OUT)
+                .build();
+
+        given(service.addReservation(eq(SEAT_NUMBER), any(SeatReservationRequest.class)))
+                .willReturn(reservation);
+
+        // when
+        ResultActions subject = mockMvc.perform(post("/seat-reservation/{seatNumber}", SEAT_NUMBER)
+//                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + )
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(request)));
+
+        // then
+        subject.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userName").value(USER_NAME))
+                .andExpect(jsonPath("$.seatNumber").value(SEAT_NUMBER))
+                .andExpect(jsonPath("$.date").value(DATE))
+                .andExpect(jsonPath("$.checkIn").value(CHECK_IN))
+                .andExpect(jsonPath("$.checkOut").value(CHECK_OUT));
+
+        // docs
+        subject.andDo(document("seat-reservation",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                        parameterWithName("seatNumber").description("좌석 번호")
+                ),
+                requestFields(
+                        fieldWithPath("userName").type(JsonFieldType.STRING).description("회원 이름"),
+                        fieldWithPath("checkIn").type(JsonFieldType.STRING).description("체크인 시간"),
+                        fieldWithPath("checkOut").type(JsonFieldType.STRING).description("체크아웃 시간")
+                ),
+                responseFields(
+                        fieldWithPath("userName").type(JsonFieldType.STRING).description("회원 이름"),
+                        fieldWithPath("seatNumber").type(JsonFieldType.NUMBER).description("좌석 번호"),
+                        fieldWithPath("date").type(JsonFieldType.STRING).description("예약 날짜"),
+                        fieldWithPath("checkIn").type(JsonFieldType.STRING).description("체크인 시간"),
+                        fieldWithPath("checkOut").type(JsonFieldType.STRING).description("체크아웃 시간")
+                )
+        ));
+    }
+
+    private String toJson(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+}
