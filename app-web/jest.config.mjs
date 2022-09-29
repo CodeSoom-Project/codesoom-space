3919b6a7e5809e0f321f08e1216e8ca0eced0801
0hu <dladudgn123@naver.com>
2022-09-30 00:13:53 +0900

    테스트 할 수 있는 환경을 만들어라
    테스트를 할 수 있게 jest와 testing library를 설치하였습니다.

---
+++ app-web/jest.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  "moduleNameMapper": {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
+}
---
+++ app-web/src/App.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react'
+import App from './App'
+
+describe('App', () => {
+  it('renders App', () => {
+    render(<App />)
+  })
+})
