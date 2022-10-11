87320f8a2e47ec4b75a108d44bc754fd885cc3af
minsuk <alstjr0183@naver.com>
2022-10-11 13:42:58 +0900

    예약내역 페이지를 퍼블리싱하라
    예약내역이라는 title을 퍼블리싱했습니다
    material-ui Button 컴포넌트를 사용하여 예약하기 버튼을 만들었습니다
    material-ui table로 만들어놓은 예약내역리스트를 렌더링했습니다

--- app-web/src/pages/Reservations.tsx
+++ app-web/src/pages/Reservations.tsx
@@ -1,7 +1,38 @@
+
+import styled from '@emotion/styled';
+
+import { Button } from '@mui/material';
+
+import ReservationsTable from '../components/reservations/ReservationsTable';
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  padding: '0 4rem',
+});
+
+const Header = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
+  marginBottom: '1rem',
+});
+
+const Title = styled.div({
+  fontSize: '2rem',
+});
+
 export default function Reservations() {
   return (
-    <div>
-      <div>Reservations</div>
-    </div>
+    <Container>
+      <Header>
+        <Title>예약내역</Title>
+        <Button style={{ fontSize: '2rem' }}>예약하기</Button>
+      </Header>
+      <ReservationsTable/>
+    </Container>
   );
 }
