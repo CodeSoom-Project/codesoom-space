4824172b7acb2d5e2f2a33075d4358e004ed262e
hxunz <guswl2843@gmail.com>
2022-10-11 22:02:16 +0900

    ✏️ 사용하지 않는 코드 삭제

--- app-web/src/pages/Reservations.tsx
+++ app-web/src/pages/Reservations.tsx
@@ -1,10 +1,7 @@
-import RetrospectionModal from './Retrospection/RetrospectionModal';
-
 export default function Reservations() {
   return (
     <div>
       <div>Reservations</div>
-      <RetrospectionModal />
     </div>
   );
 }
