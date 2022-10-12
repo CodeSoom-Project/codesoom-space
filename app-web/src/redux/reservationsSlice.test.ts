eb7f4912077521219e26fa01e8c053eee158ce32
minsuk <alstjr0183@naver.com>
2022-10-12 11:10:57 +0900

    예약모달 렌더링여부를 결정하는 상태를 반대로 변경하는 test를 작성하라
    예약모달 렌더링상태를 결정하는 isOpenRetrospectModal state를 반대로 변경하는 액션의 test를 작성했습니다

--- app-web/src/redux/reservationsSlice.test.ts
+++ app-web/src/redux/reservationsSlice.test.ts
@@ -1,6 +1,7 @@
 import reducer, {
   initialState,
   toggleReservationsModal,
+  toggleRetrospectModal,
 } from './reservationsSlice';
 
 describe('toggleReservationsModal', () => {
@@ -10,4 +11,14 @@ describe('toggleReservationsModal', () => {
     expect(state.isOpenReservationsModal).toBe(!initialState.isOpenReservationsModal);
   });
 });
+
+describe('toggleRetrospectModal', () => {
+  it('isOpenRetrospectModal을 반대로 변경한다', () => {
+    const state = reducer(initialState, toggleRetrospectModal());
+
+    expect(state.isOpenRetrospectModal).toBe(!initialState.isOpenRetrospectModal);
+  });
+});
+
+
 
