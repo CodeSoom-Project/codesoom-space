448dd8a4d06616f8ac7d58167cf6fc1f01c531c5
subin606 <gaemilee606@gmail.com>
2022-10-13 17:37:22 +0900

    파라미터의 순서를 변경하라

--- app-server/app/src/main/java/com/codesoom/myseat/services/ReservationQueryService.java
+++ app-server/app/src/main/java/com/codesoom/myseat/services/ReservationQueryService.java
@@ -34,13 +34,13 @@ public class ReservationQueryService {
     /**
      * 주어진 user id와 reservation id로 예약 내용을 상세 조회합니다.
      *
-     * @param userId 유저 아이디(PK)
+     * @param userId        유저 아이디(PK)
      * @param reservationId 예약 아이디
      * @return 주어진 정보로 조회한 예약 정보
      * @throws ReservationNotFoundException 예약 정보를 찾지 못한 경우 던짐
      */
     @Transactional(readOnly = true)
-    public Reservation reservation(Long userId, Long reservationId) {
+    public Reservation reservation(Long reservationId, Long userId) {
         return repository.findByIdAndUser_Id(reservationId, userId)
                 .orElseThrow(ReservationNotFoundException::new);
     }
