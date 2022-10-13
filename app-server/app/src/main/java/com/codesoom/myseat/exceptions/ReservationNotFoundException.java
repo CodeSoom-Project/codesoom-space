50f97794560e33eab36c786b9fc0428eee63b20c
subin606 <gaemilee606@gmail.com>
2022-10-13 17:33:25 +0900

    static final 변수명을 대문자로 변경하라

--- app-server/app/src/main/java/com/codesoom/myseat/exceptions/ReservationNotFoundException.java
+++ app-server/app/src/main/java/com/codesoom/myseat/exceptions/ReservationNotFoundException.java
@@ -3,10 +3,10 @@ package com.codesoom.myseat.exceptions;
 /** 예약을 찾지 못할 경우 던지는 예외 */
 public class ReservationNotFoundException extends RuntimeException {
 
-    private static final String default_message = "예약을 찾지 못했습니다.";
+    private static final String DEFAULT_MESSAGE = "예약을 찾지 못했습니다.";
 
     public ReservationNotFoundException() {
-        super(default_message);
+        super(DEFAULT_MESSAGE);
     }
 
     public ReservationNotFoundException(String message) {
