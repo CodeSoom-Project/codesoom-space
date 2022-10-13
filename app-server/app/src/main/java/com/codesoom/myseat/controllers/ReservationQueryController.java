74057eeee3a79d771ab64fe07d53c0b0b776792c
subin606 <gaemilee606@gmail.com>
2022-10-13 16:01:48 +0900

    예약 조회 컨트롤러를 추가하라

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/ReservationQueryController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/ReservationQueryController.java
@@ -3,13 +3,16 @@ package com.codesoom.myseat.controllers;
 import com.codesoom.myseat.domain.User;
 import com.codesoom.myseat.dto.ReservationListResponse;
 import com.codesoom.myseat.dto.ReservationResponse;
+import com.codesoom.myseat.exceptions.ReservationNotFoundException;
 import com.codesoom.myseat.security.UserAuthentication;
 import com.codesoom.myseat.services.ReservationQueryService;
 import com.codesoom.myseat.services.UserService;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +35,10 @@ public class ReservationQueryController {
     }
 
     /**
-     * 예약 목록을 조회합니다.
+     * 요청한 회원의 모든 예약 목록을 조회합니다.
+     *
+     * @param principal 회원 인증 정보
+     * @return 회원의 모든 예약 정보
      */
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
@@ -47,6 +53,25 @@ public class ReservationQueryController {
                 .map(ReservationResponse::new)
                 .collect(Collectors.toList())
         );
+    }
+
+    /**
+     * 예약 id와 회원 id로 예약 내용을 상세 조회합니다.
+     *
+     * @param principal 회원 인증 정보
+     * @param reservationId 예약 id
+     * @return 예약 정보
+     * @throws ReservationNotFoundException 주어진 예약 id와 회원 id로 예약 정보를 찾지 못한 경우
+     */
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public ReservationResponse reservation(@AuthenticationPrincipal UserAuthentication principal,
+                                           @PathVariable(name = "id") int reservationId) {
+        String email = principal.getEmail();
+        User user = userService.findUser(email);
+
+        return new ReservationResponse(service.reservation(user.getId(), Long.valueOf(reservationId)));
     }
 
 }
