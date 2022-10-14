fe1608b02cf4beccd07fb2c8ee46bb5575334bf8
hxunz <guswl2843@gmail.com>
2022-10-14 20:38:44 +0900

    🎨 회원가입 페이지 디자인 수정

--- app-web/src/logIn.tsx
+++ app-web/src/logIn.tsx
@@ -11,52 +11,51 @@ export default function LogIn({ register, errors, handleSubmit, login }:
 
   return (
     <LoginBox >
-      <main style={{ padding: '1rem 0' }}>
-        <form
-          onSubmit={handleSubmit(async (data: any) => {
-            await login({
-              email: data.email,
-              password: data.password,
-            });
-          })
-          }>
+      <form
+        style={{ padding: '1rem 0' }}
+        onSubmit={handleSubmit(async (data: any) => {
+          await login({
+            email: data.email,
+            password: data.password,
+          });
+        })
+        }>
 
-          <table>
-            <tbody>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <div>
-                  <tr>
-                    <td><label htmlFor="email">Email</label></td>
-                    <td>
-                      <input {...register('email', {
-                        required: true,
-                      },
-                      )} type="email" id="email" />
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td><label htmlFor="password">비밀번호</label></td>
-                    <td>
-                      <input {...register('password', {
-                        required: true,
-                      })} type="password" id="password" />
-                    </td>
-                  </tr>
-
-                  {errors.name?.type === 'required' && '이름을 입력 해 주세요'}
-                  {errors.password?.type === 'required' && '비밀번호를 입력 해 주세요'}
-                </div>
+        <table>
+          <tbody>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div>
                 <tr>
+                  <td><label htmlFor="email">Email</label></td>
                   <td>
-                    <Button type="submit" variant="outlined">로그인</Button>
+                    <input {...register('email', {
+                      required: true,
+                    },
+                    )} type="email" id="email" />
                   </td>
                 </tr>
+
+                <tr>
+                  <td><label htmlFor="password">비밀번호</label></td>
+                  <td>
+                    <input {...register('password', {
+                      required: true,
+                    })} type="password" id="password" />
+                  </td>
+                </tr>
+
+                {errors.name?.type === 'required' && '이름을 입력 해 주세요'}
+                {errors.password?.type === 'required' && '비밀번호를 입력 해 주세요'}
               </div>
-            </tbody>
-          </table>
-        </form>
-      </main>
+              <tr>
+                <td>
+                  <Button type="submit" variant="outlined">로그인</Button>
+                </td>
+              </tr>
+            </div>
+          </tbody>
+        </table>
+      </form>
     </LoginBox>
   );
 }
