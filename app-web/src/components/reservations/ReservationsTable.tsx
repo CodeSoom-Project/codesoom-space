0b6e991bc0b90107442cdab91636aa751e8593a4
hxunz <guswl2843@gmail.com>
2022-10-14 16:08:07 +0900

    🎨 예약 모달과 회고 모달 디자인 통일 작업

--- app-web/src/components/reservations/ReservationsTable.tsx
+++ app-web/src/components/reservations/ReservationsTable.tsx
@@ -98,6 +98,7 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
     </Box>
   );
 }
+
 interface Props {
   onOpenReservationModal: React.ReactEventHandler,
   onOpenRetrospectModal: React.ReactEventHandler
