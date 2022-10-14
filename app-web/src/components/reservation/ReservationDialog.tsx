ef9b32637de38e6ccbb0b61aa8b74083cf0ae375
minsuk <alstjr0183@naver.com>
2022-10-14 22:12:31 +0900

    상세페이지와 예약하기 페이지를 다르게 보이게하라
    예약하기 클릭시는 계획을 신청할수있는 Form이 보이고
    상세페이지클릭시 신청한 정보를 확인할수있는 text가 보이게끔했습니다

--- app-web/src/components/reservation/ReservationDialog.tsx
+++ app-web/src/components/reservation/ReservationDialog.tsx
@@ -23,6 +23,7 @@ import { get } from '../../utils';
 import { saveDate, saveContent } from '../../redux/reservationsSlice';
 
 import { getReservations, reservationsKeys } from '../../services/reservations';
+import { CircularProgress } from '@mui/material';
 
 const TextFieldWrap = styled.div({
   display: 'flex',
@@ -48,30 +49,37 @@ const Text = styled(TextField)({
   margin: '1rem 3rem 1rem 0',
 });
 
+const DateTitle = styled.h1({
+  margin: '0',
+  fontSize: '1.3rem',
+});
+
+const TextTitle = styled.h1({
+  marginBottom: '1rem',
+  fontSize: '1.3rem',
+});
+
+const TextBox = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  maxHeight: '20rem',
+  overflow: 'auto',
+  marginBottom: '2rem',
+
+  'p': {
+    margin: '0 0 1rem 0',
+  },
+});
+
 interface Props {
-  open: boolean,
   onClose: React.ReactEventHandler,
   onApply: React.ReactEventHandler
 }
 
-export default function ReservationDialog({ open, onClose, onApply }: Props) {
+export default function ReservationDialog({ onClose, onApply }: Props) {
   const dispatch = useDispatch();
 
-  const { date, content } = useAppSelector(get('reservations'));
-
-  const id = 2;
-
-  const { isLoading, data, isError } = useQuery(
-    reservationsKeys.reservationsById(id),
-    () => getReservations(id), { retry: 1 });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error</div>;
-  }
+  const { date, content, id, isDetail } = useAppSelector(get('reservations'));
 
   const handleChange = (value: dayjs.Dayjs | null) => {
     dispatch(
@@ -79,40 +87,64 @@ export default function ReservationDialog({ open, onClose, onApply }: Props) {
     );
   };
 
+  const { isLoading, data } = useQuery(
+    reservationsKeys.reservationsById(id),
+    () => getReservations(id), { retry: 1 });
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
   return (
     <Dialog
-      open={open}
+      open={true}
       onClose={onClose}
       aria-labelledby="form-dialog-title"
     >
-      <Title>
-        공부방 예약하기
-      </Title>
+      {!isDetail && (
+        <Title>
+          공부방 예약하기
+        </Title>
+      )}
+
 
       <TextFieldWrap>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label="방문일자"
-            value={date === null ? null : dayjs(date)}
-            onChange={(value) => handleChange(value)}
-            renderInput={(params) => {
-              return (<TextField {...params} />);
-            }}
-          />
-        </LocalizationProvider>
+        {isDetail && (<DateTitle>예약일 : {data.date}</DateTitle>)}
+        {!isDetail && (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="방문일자"
+              value={date === null ? null : dayjs(date)}
+              onChange={(value) => handleChange(value)}
+              renderInput={(params) => {
+                return (<TextField {...params} />);
+              }}
+            />
+          </LocalizationProvider>
+        )}
 
-        <Text
-          label="계획"
-          value={content}
-          onChange={(e) => {
-            dispatch(saveContent(e.target.value));
-          }}
-          variant="outlined"
-          multiline
-          rows={3}
-          placeholder="계획을 입력해주세요."
-          fullWidth
-        />
+        {isDetail && (
+          <>
+            <TextTitle>계획</TextTitle>
+            <TextBox>{data.content.split('\n').map((line:string) => (<p>{line}</p>))}</TextBox>
+          </>
+        )
+        }
+        {!isDetail && (
+          <Text
+            label="계획"
+            value={content}
+            onChange={(e) => {
+              dispatch(saveContent(e.target.value));
+            }}
+            variant="outlined"
+            multiline
+            rows={3}
+            placeholder="계획을 입력해주세요."
+            fullWidth
+          />
+        )}
+
         <ButtonWrap>
           <Button variant="outlined" size="small" onClick={onClose}>
             취소
