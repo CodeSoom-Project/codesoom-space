5e5ecafc9c8bf8475a4e4be9236813f7eda9625c
0hu <dladudgn123@naver.com>
2022-10-18 11:21:12 +0900

    headerContainer에서 토큰을 감지하라
    현재 App 에서 useEffect를 활용하여 토큰을 관리를 하는 중에 HeaderContainer를 여러곳에서 랜더링 하고 있어 HeaderContainer에서  토큰을 관리하는게 맞다고 생각하였습니다.

--- app-web/src/App.tsx
+++ app-web/src/App.tsx
@@ -1,26 +1,8 @@
 import './App.css';
 
-import { useEffect } from 'react';
-
-import { loadItem } from './services/stoage';
-
-import { useAppDispatch } from './hooks';
-
-import { setAccessToken } from './authSlice';
-
 import HeaderContainer from './HeaderContainer';
 
 export default function App() {
-  const dispatch = useAppDispatch();
-
-  const accessToken = loadItem('accessToken');
-
-  useEffect(() => {
-    if (accessToken) {
-      dispatch(setAccessToken(accessToken));
-    }
-  }, [accessToken]);
-
   return (
     <div>
       <HeaderContainer/>
--- app-web/src/HeaderContainer.tsx
+++ app-web/src/HeaderContainer.tsx
@@ -1,12 +1,28 @@
-import { useDispatch } from 'react-redux';
-import { logout } from './authSlice';
-import Header from './Header';
+import { useEffect } from 'react';
+
 import { useAppSelector } from './hooks';
+import { useDispatch } from 'react-redux';
+
+import { get } from './utils';
+
+import { logout, setAccessToken } from './authSlice';
+
+import Header from './Header';
+
+import { loadItem } from './services/stoage';
 
 export default function HeaderContainer() {
-  const accessToken = useAppSelector((state) => state.auth.accessToken);
+  const { accessToken } = useAppSelector(get('auth'));
 
   const dispatch = useDispatch();
+
+  const token = loadItem('accessToken');
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setAccessToken(token));
+    }
+  }, [token]);
 
   function handleLogout() {
     dispatch(logout());
