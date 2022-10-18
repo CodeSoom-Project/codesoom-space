22eec2416a4b7bfb087ff1d3f38084b18fbae775
0hu <dladudgn123@naver.com>
2022-10-18 13:26:51 +0900

    앱이 실행되었을 때 토큰을 가져와라
    이전 커밋에서 headerContainer 가 마운트 될때 마다 토큰을 가져왔습니다.
    앱이 실행되었을 떄 토큰을 가져오게 App 컴포넌트로 옮겼습니다.

--- app-web/src/App.tsx
+++ app-web/src/App.tsx
@@ -1,8 +1,26 @@
 import './App.css';
 
+import { useEffect } from 'react';
+
+import { useAppDispatch } from './hooks';
+
+import { setAccessToken } from './authSlice';
+
 import HeaderContainer from './HeaderContainer';
 
+import { loadItem } from './services/stoage';
+
 export default function App() {
+  const dispatch = useAppDispatch();
+
+  const accessToken = loadItem('accessToken');
+
+  useEffect(() => {
+    if (accessToken) {
+      dispatch(setAccessToken(accessToken));
+    }
+  }, [accessToken]);
+
   return (
     <div>
       <HeaderContainer/>
--- app-web/src/HeaderContainer.tsx
+++ app-web/src/HeaderContainer.tsx
@@ -1,28 +1,16 @@
-import { useEffect } from 'react';
-
 import { useAppSelector } from './hooks';
 import { useDispatch } from 'react-redux';
 
 import { get } from './utils';
 
-import { logout, setAccessToken } from './authSlice';
+import { logout } from './authSlice';
 
 import Header from './Header';
-
-import { loadItem } from './services/stoage';
 
 export default function HeaderContainer() {
   const { accessToken } = useAppSelector(get('auth'));
 
   const dispatch = useDispatch();
-
-  const token = loadItem('accessToken');
-
-  useEffect(() => {
-    if (token) {
-      dispatch(setAccessToken(token));
-    }
-  }, [token]);
 
   function handleLogout() {
     dispatch(logout());
