a314a6412a5a7d3ed69c0c45200ce33957f49362
jihwooon <jihwooon@gmail.com>
2022-10-20 16:58:47 +0900

    RetrospectiveServiceTest에 createRetrospective 메서드명을 변경하라.
    상태 코드를 완료로 변경하도록 메서드를 구현했습니다

--- app-server/app/src/main/java/com/codesoom/myseat/domain/Reservation.java
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/Reservation.java
@@ -77,4 +77,8 @@ public class Reservation {
         this.status = ReservationStatus.CANCELED;
     }
 
+    public void completeRetrospective() {
+        this.status = ReservationStatus.RETROSPECTIVE_COMPLETE;
+    }
+
 }
