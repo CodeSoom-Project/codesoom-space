ede991b7d63566341844d46a6cd7fecf2cbbcb81
hxunz <guswl2843@gmail.com>
2022-10-21 10:59:27 +0900

    ✏️ import문을 구분하기 위해서 공백 줄 추가

--- app-web/src/components/reservations/ReservationsTable.tsx
+++ app-web/src/components/reservations/ReservationsTable.tsx
@@ -33,7 +33,9 @@ import FirstPageIcon from '@mui/icons-material/FirstPage';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import LastPageIcon from '@mui/icons-material/LastPage';
+
 import { useMutation } from 'react-query';
+
 import { cancelReservation } from '../../services/reservations';
 
 interface TablePaginationActionsProps {
