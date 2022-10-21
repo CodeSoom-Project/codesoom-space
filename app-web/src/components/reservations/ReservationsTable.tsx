6e100429ae98e6129c64502f6ee374297cbeab23
hxunz <guswl2843@gmail.com>
2022-10-21 18:23:45 +0900

    🐛 lint 에러 해결

--- app-web/src/components/reservations/ReservationsTable.tsx
+++ app-web/src/components/reservations/ReservationsTable.tsx
@@ -40,7 +40,7 @@ interface TablePaginationActionsProps {
   onPageChange: (
     event: React.MouseEvent<HTMLButtonElement>,
     newPage: number,
-  ) => void;
+  )=> void;
 }
 
 const StyledTableCell = styled(TableCell)({
