c7899c9977ad30abd38b929b7011acc57bdd0d00
Woo Dy <kjop7419@naver.com>
2022-10-21 17:03:55 +0900

    로그인을 요청해 토큰을 저장하라
    로그인 요청을 보낸 후, redux와 localStorage에 토큰을 저장하는 액션을 추가했습니다.

--- app-web/src/logInContainer.tsx
+++ app-web/src/logInContainer.tsx
@@ -4,22 +4,34 @@ import { useForm } from 'react-hook-form';
 
 import { useMutation } from 'react-query';
 
+import { useDispatch } from 'react-redux';
+
 import { login } from './services/api';
 
+
 import LogIn from './logIn';
+import { saveItem } from './services/stoage';
+import { setAccessToken } from './redux/authSlice';
 
 export default function LogInContainer() {
+  const dispatch = useDispatch();
+
   const { register, formState: { errors }, handleSubmit } = useForm();
 
   const navigate = useNavigate();
 
-  const loginMutate = async ({ email, password }: { email: any, password: string }) => {
+  const loginMutate =
+  async ({ email, password }: { email: string, password: string }) => {
     const accessToken = await login({ email, password });
     return accessToken;
   };
 
   const { mutate } = useMutation('login', loginMutate, {
-    onSuccess: async () => {
+    onSuccess: async (accessToken) => {
+      saveItem('accessToken', accessToken);
+
+      dispatch(setAccessToken(accessToken));
+
       navigate('/', { replace: true });
     },
     onError: async (e) => {
