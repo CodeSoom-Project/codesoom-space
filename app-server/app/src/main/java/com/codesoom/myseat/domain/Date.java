05763586e150b68093e4daf0516b0a171e8f9a1c
subin606 <gaemilee606@gmail.com>
2022-10-21 20:49:40 +0900

    예약 가능한 날짜인지 확인하는 함수를 추가하라
    현재 예약 정책상 오늘날짜보다 과거의 날짜,
    당일 예약, 평일 예약이 불가합니다.
    해당 조건에 규합하는지 확인하는 메서드를 추가했습니다.

--- app-server/app/src/main/java/com/codesoom/myseat/domain/Date.java
+++ app-server/app/src/main/java/com/codesoom/myseat/domain/Date.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Date {
 
@@ -18,21 +19,39 @@ public class Date {
     }
 
     /**
-     * 주어진 날짜가 오늘보다 과거인지 확인합니다.
+     * 예약 가능한 날짜인지 여부를 확인합니다.
+     *
+     * @return 예약이 가능한 날짜라면 true, 아니라면 false
+     */
+    public boolean isReservable() {
+        return !isBeforeDate() && !isNow() && isWeekend();
+    }
+
+    /**
+     * 날짜가 오늘보다 과거인지 확인합니다.
      *
      * @return 오늘보다 과거라면 true, 아니라면 false
      */
-    public boolean isBeforeDate() {
+    private boolean isBeforeDate() {
         return toLocalDate().isBefore(LocalDate.now());
     }
 
     /**
-     * 주어진 날짜의 요일이 주말인지 확인합니다.
+     * 날짜의 요일이 주말인지 확인합니다.
      *
      * @return 토요일 혹은 일요일이라면 true, 아니라면 false
      */
-    public boolean isWeekend() {
+    private boolean isWeekend() {
         return toLocalDate().getDayOfWeek().getValue() > 5;
+    }
+
+    /**
+     * 날짜가 오늘 날짜와 동일한지 확인합니다.
+     *
+     * @return 오늘 날짜와 동일하면 true, 아니라면 false
+     */
+    private boolean isNow() {
+        return toLocalDate().equals(LocalDate.now());
     }
 
     /**
@@ -42,6 +61,19 @@ public class Date {
      */
     private LocalDate toLocalDate() {
         return LocalDate.parse(this.date, DATE_FORMAT);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date1 = (Date) o;
+        return Objects.equals(date, date1.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date);
     }
 
 }
