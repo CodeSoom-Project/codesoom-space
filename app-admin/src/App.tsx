ca36aa307bd6531867bca56ef09cee50d2c35b7c
Woo Dy <kjop7419@naver.com>
2022-10-25 17:59:32 +0900

    Route를 연결하라
    로그인 페이지와 reservations페이지를 연결하는 작업을 진행했습니다.

--- app-admin/src/App.tsx
+++ app-admin/src/App.tsx
@@ -1,7 +1,13 @@
-export default function App () {
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Reservations from './pages/Reservations';
+
+export default function App() {
   return (
-    <>
-      <h1>좌석 예약 어드민</h1>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='reservations' element={<Reservations/>}/>
+    </Routes>
+  );
 }
