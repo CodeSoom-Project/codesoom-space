7ca697c803b6cbab56d5caea76357a5385e40c0c
HanJiyoung <jiyounghaan@gmail.com>
2022-10-25 17:35:24 +0900

    /mypage 요청시 회원 정보를 응답하라

---
+++ app-server/app/src/test/java/com/codesoom/myseat/controllers/mypage/MyPageControllerTest.java
@@ -0,0 +1,61 @@
+package com.codesoom.myseat.controllers.mypage;
+
+import com.codesoom.myseat.domain.User;
+import com.codesoom.myseat.services.auth.AuthenticationService;
+import com.codesoom.myseat.services.users.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(MyPageController.class)
+class MyPageControllerTest {
+    private static final String ACCESS_TOKEN
+            = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authService;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @DisplayName("GET /mypage 요청 시 상태코드 200과 함께 회원 정보를 응답한다")
+    void GET_mypage_responses_status_code_204_and_returns_user_data() throws Exception {
+        User mockUser = User.builder()
+                .id(1L)
+                .name("김철수")
+                .email("soo@email.com")
+                .password("$2a$10$hxqWrlGa7SQcCEGURjmuQup4J9kN6qnfr4n7j7R3LvzHEoEOUTWeW")
+                .build();
+
+        given(authService.parseToken(ACCESS_TOKEN))
+                .willReturn(1L);
+
+        given(userService.findById(1L))
+                .willReturn(mockUser);
+
+        ResultActions result =  mockMvc.perform(get("/mypage")
+                .header("Authorization", "Bearer " + ACCESS_TOKEN));
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("김철수"))
+                .andExpect(jsonPath("$.email").value("soo@email.com"));
+    }
+
+}
