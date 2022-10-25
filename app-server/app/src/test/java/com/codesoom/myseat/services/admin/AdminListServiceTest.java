b354e7708b1f3ddbcb8c3a24bec2d67c4b7b0cd3
jihwooon <jihwooon@gmail.com>
2022-10-25 21:03:32 +0900

    AdminListServiceTest에 테스트코드를 작성하라.
    관리자가 회원의 모든 예약 목록을 조회한다 하는 테스트코드를 작성했습니다.

---
+++ app-server/app/src/test/java/com/codesoom/myseat/services/admin/AdminListServiceTest.java
@@ -0,0 +1,61 @@
+package com.codesoom.myseat.services.admin;
+
+import com.codesoom.myseat.domain.Date;
+import com.codesoom.myseat.domain.Plan;
+import com.codesoom.myseat.domain.Reservation;
+import com.codesoom.myseat.domain.User;
+import com.codesoom.myseat.enums.ReservationStatus;
+import com.codesoom.myseat.repositories.ReservationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+class AdminListServiceTest {
+
+    private AdminListService adminListService;
+
+    @Mock
+    private ReservationRepository reservationRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        adminListService = new AdminListService(reservationRepository);
+    }
+
+    @DisplayName("회원의 모든 예약 목록을 조회한다")
+    @Test
+    void will_return_reservations() {
+        User mockUser = User.builder()
+                .id(1L)
+                .name("김철수")
+                .build();
+        Plan plan = Plan.builder().content("코테풀기").build();
+        Reservation reservation = Reservation.builder()
+                .id(1L)
+                .user(mockUser)
+                .date(new Date("2022-10-12"))
+                .status(ReservationStatus.valueOf("RETROSPECTIVE_COMPLETE"))
+                .plan(plan)
+                .build();
+
+        given(reservationRepository.findAll())
+                .willReturn(List.of(reservation));
+
+        List<Reservation> reservations = adminListService.reservations();
+
+        assertThat(reservations.size()).isEqualTo(1);
+
+        assertThat(reservations.get(0).getUser().getName()).isEqualTo("김철수");
+        assertThat(reservations.get(0).getDate()).isEqualTo(new Date("2022-10-12"));
+        assertThat(reservations.get(0).getStatus()).isEqualTo(ReservationStatus.valueOf("RETROSPECTIVE_COMPLETE"));
+    }
+}
