583adfab3447ecd21a90b6da8663a2fcd2121a5c
subin606 <gaemilee606@gmail.com>
2022-10-25 18:05:08 +0900

    예약 불가능한 날짜에 대한 컨트롤러 테스트를 추가하라

--- app-server/app/src/test/java/com/codesoom/myseat/controllers/reservations/ReservationAddControllerTest.java
+++ app-server/app/src/test/java/com/codesoom/myseat/controllers/reservations/ReservationAddControllerTest.java
@@ -1,8 +1,13 @@
 package com.codesoom.myseat.controllers.reservations;
 
+import com.codesoom.myseat.domain.Date;
+import com.codesoom.myseat.domain.Plan;
+import com.codesoom.myseat.domain.Reservation;
 import com.codesoom.myseat.domain.User;
 import com.codesoom.myseat.dto.ReservationRequest;
+import com.codesoom.myseat.enums.ReservationStatus;
 import com.codesoom.myseat.exceptions.AlreadyReservedException;
+import com.codesoom.myseat.exceptions.NotReservableDateException;
 import com.codesoom.myseat.services.auth.AuthenticationService;
 import com.codesoom.myseat.services.reservations.ReservationAddService;
 import com.codesoom.myseat.services.users.UserService;
@@ -18,6 +23,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -41,9 +49,27 @@ class ReservationAddControllerTest {
     @MockBean
     private ReservationAddService reservationAddService;
 
+    Date getReservableDate() {
+        long plusDays = 0;
+        LocalDate now = LocalDate.now();
+        if (now.getDayOfWeek().getValue() < 6) {
+            plusDays = 6 - now.getDayOfWeek().getValue();
+        }
+        LocalDate saturday = now.plusDays(plusDays);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return new Date(saturday.format(dateFormat));
+    }
+
+    Date getNotReservableDate() {
+        LocalDate notReservableDate =  LocalDate.now().minusDays(1);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return new Date(notReservableDate.format(dateFormat));
+    }
+
     @Test
     @DisplayName("POST /reservations 요청 시 상태코드 204를 응답한다")
     void POST_reservations_responses_status_code_204() throws Exception {
+        Date date = getReservableDate();
         User mockUser = User.builder()
                 .id(1L)
                 .name("김철수")
@@ -58,9 +84,17 @@ class ReservationAddControllerTest {
                 .willReturn(mockUser);
 
         ReservationRequest request = ReservationRequest.builder()
-                .date("2022-10-11")
+                .date(date.getDate())
                 .content("책읽기, 코테풀기")
                 .build();
+
+        given(reservationAddService.createReservation(mockUser, date.getDate(), "책읽기, 코테풀기"))
+                .willReturn(Reservation.builder()
+                        .user(mockUser)
+                        .plan(Plan.builder().content("책읽기, 코테풀기").build())
+                        .date(date)
+                        .status(ReservationStatus.RESERVED)
+                        .build());
 
         ResultActions result =  mockMvc.perform(post("/reservations")
                 .header("Authorization", "Bearer " + ACCESS_TOKEN)
@@ -73,6 +107,7 @@ class ReservationAddControllerTest {
     @Test
     @DisplayName("이미 예약 내역이 존재하는 방문 일자가 주어지면 상태코드 400을 응답한다")
     void If_already_existing_date_given_it_responses_status_code_400() throws Exception {
+        Date date = getReservableDate();
         User mockUser = User.builder()
                 .id(1L)
                 .name("김철수")
@@ -86,11 +121,44 @@ class ReservationAddControllerTest {
         given(userService.findById(1L))
                 .willReturn(mockUser);
 
-        given(reservationAddService.createReservation(mockUser, "2022-10-11", "책읽기, 코테풀기"))
+        given(reservationAddService.createReservation(mockUser, date.getDate(), "책읽기, 코테풀기"))
                 .willThrow(AlreadyReservedException.class);
 
         ReservationRequest request = ReservationRequest.builder()
-                .date("2022-10-11")
+                .date(date.getDate())
+                .content("책읽기, 코테풀기")
+                .build();
+
+        ResultActions result = mockMvc.perform(post("/reservations")
+                .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(request)));
+
+        result.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("예약이 불가능한 날짜가 주어지면 상태코드 400을 응답한다")
+    void If_not_reservable_date_given_it_responses_status_code_400() throws Exception {
+        Date notReservableDate = getNotReservableDate();
+        User mockUser = User.builder()
+                .id(1L)
+                .name("김철수")
+                .email("soo@email.com")
+                .password("$2a$10$hxqWrlGa7SQcCEGURjmuQup4J9kN6qnfr4n7j7R3LvzHEoEOUTWeW")
+                .build();
+
+        given(authService.parseToken(ACCESS_TOKEN))
+                .willReturn(1L);
+
+        given(userService.findById(1L))
+                .willReturn(mockUser);
+
+        given(reservationAddService.createReservation(mockUser,notReservableDate.getDate(), "책읽기, 코테풀기"))
+                .willThrow(NotReservableDateException.class);
+
+        ReservationRequest request = ReservationRequest.builder()
+                .date(notReservableDate.getDate())
                 .content("책읽기, 코테풀기")
                 .build();
 
