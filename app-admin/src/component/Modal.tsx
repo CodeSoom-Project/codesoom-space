d3f9312d4cccf5ac72180a5791ef7e04e77ecc91
minsuk <alstjr0183@naver.com>
2022-10-26 12:49:54 +0900

    여러곳에서 사용할수있는 모달을 만들어라
    공통적으로 쓸수있는 모달을 만들었습니다
    필수속성
      open
      content
      onClose
      onClick
    선택속성
      title
      text

---
+++ app-admin/src/component/Modal.tsx
@@ -0,0 +1,33 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material';
+
+interface Props {
+  open: boolean;
+
+  title?: string;
+  content: string;
+  text?: string;
+
+  onClose: React.ReactEventHandler;
+  onClick: React.ReactEventHandler;
+}
+
+export default function Modal({ open, title, content, text = '확인', onClose, onClick }: Props) {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+    >
+      <DialogTitle >
+        {title}
+      </DialogTitle>
+
+      <DialogContent sx={{ whiteSpace: 'pre-line' }}>
+        {content}
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={onClick}>{text}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
