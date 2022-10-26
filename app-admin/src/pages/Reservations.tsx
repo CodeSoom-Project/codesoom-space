bd45bfb9033c20032be4663ecbf8150489734ca7
minsuk <alstjr0183@naver.com>
2022-10-26 18:16:47 +0900

    코드를 복구하라
    테스트로 사용한 코드를 다시 되돌렸습니다

--- app-admin/src/pages/Reservations.tsx
+++ app-admin/src/pages/Reservations.tsx
@@ -1,15 +1,3 @@
-import Modal from '../component/Modal';
-
 export default function Reservations() {
-  return (
-    <div>
-      <Modal
-        open={true}
-        title="계획내용"
-        content={'오늘은 자바스크립트를 공부해보겠습니다 \n git 공부를 해보겠습니다'}
-        onClose={() => {}}
-        onClick={() => {}}
-      />
-    </div>
-  );
+  return <div>Reservations</div>;
 }
