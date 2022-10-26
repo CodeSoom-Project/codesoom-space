19a6a95e121d634b981de7bc57a9936c2a27843c
hannut91 <hannut91@gmail.com>
2022-10-26 14:17:57 +0900

    예약 목록에 페이지네이션 정보를 포함시켜라
    예약 목록에 페이지네이션 정보를 포함시키기 위해
    `ReservationListPageResponse`를 정의했습니다.

---
+++ app-server/app/src/main/java/com/codesoom/myseat/dto/ReservationListPageResponse.java
@@ -0,0 +1,30 @@
+package com.codesoom.myseat.dto;
+
+import com.codesoom.myseat.domain.Reservation;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/** 예약 목록 조회 응답 정보 */
+@NoArgsConstructor
+@Getter
+public class ReservationListPageResponse {
+
+    private List<ReservationResponse> reservations;
+    private Pagination pagination;
+
+    public ReservationListPageResponse(Page<Reservation> pageReservation) {
+        this.reservations = pageReservation.getContent().stream()
+                .map(ReservationResponse::new)
+                .collect(Collectors.toList());
+        this.pagination = new Pagination(
+                pageReservation.getNumber() + 1,
+                pageReservation.getSize(),
+                pageReservation.getTotalPages()
+        );
+    }
+
+}
