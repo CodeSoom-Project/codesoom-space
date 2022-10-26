0969d106eac6f1c0c030372bb74164ab4cb19389
minsuk <alstjr0183@naver.com>
2022-10-26 10:33:19 +0900

    함수를 한줄로 표현하라
    기존에 return을 사용하여 함수코드가 4줄로 표현됬었는데
    화살표함수를 사용하고 한줄일때는 return을 생략할수있기때문에
    더욱 간결한 형태로 수정했습니다

--- app-admin/src/App.test.tsx
+++ app-admin/src/App.test.tsx
@@ -3,11 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  function renderApp() {
-    return render((
-      <App />
-    ));
-  }
+  const renderApp = () => render(<App/>);
 
   it('좌석 예약 어드민이 보인다', () => {
     const { container } = renderApp();
