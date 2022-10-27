a86243b03a2c39713817f2df9db9f018c2ea9f61
Woo Dy <kjop7419@naver.com>
2022-10-27 21:04:55 +0900

    fixture데이터와 column에 쓰일 데이터를 생성하라
    안전한 테스트를 위한 fixture데이터를 생성하고
    column에서 쓰이는 데이터를 생성했습니다.

---
+++ app-admin/src/data/column.ts
@@ -0,0 +1,13 @@
+export const column = [{
+  id: 1,
+  label: '이름',
+}, {
+  id: 2,
+  label: '예약 날짜',
+}, {
+  id: 3,
+  label: '계획',
+}, {
+  id: 4,
+  label: '회고',
+}];
---
+++ app-admin/src/fixtures/reservations.ts
@@ -0,0 +1,29 @@
+export const reservations = [{
+  id: 1,
+  date: '2022-10-25',
+  content: '알고리즘 풀기',
+  status: 'RETROSPECTIVE_WATING',
+  user: {
+    id: 1,
+    name: '홍길동',
+  },
+}, {
+  id: 2,
+  date: '2022-10-26',
+  content: '스프링 공부',
+  status: 'RETROSPECTIVE_COMPLETE',
+  user: {
+    id: 2,
+    name: '양민석',
+  },
+}, {
+  id: 3,
+  date: '2022-10-27',
+  content: '리액트 공부',
+  status: 'RETROSPECTIVE_WATING',
+  user: {
+    id: 3,
+    name: '김현지',
+  },
+},
+];
