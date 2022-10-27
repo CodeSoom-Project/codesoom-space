ab33a9754593d01182c342fd6bf2c3554a2e0cce
yerin512 <sosu0512@naver.com>
2022-10-27 16:43:48 +0900

    이메일 인증확인 기본 화면은 로딩되는 UI를 보여줘라
    기본 화면은 로딩화면으로 했습니다.

--- app-web/src/pages/Verification/Email/index.tsx
+++ app-web/src/pages/Verification/Email/index.tsx
@@ -1,6 +1,19 @@
+import { styled } from '@mui/material/styles';
+import {
+  Box,
+  CircularProgress,
+} from '@mui/material';
+
+const StyledBox = styled(Box)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 export default function Email() {
   return (
-    <p>이메일 검사</p>
+    <StyledBox>
+      <CircularProgress />
+    </StyledBox>
   );
 }
