906a3be8d02f4dd1709456082484e1f3a96c232f
HanJiyoung <jiyounghaan@gmail.com>
2022-10-27 21:03:57 +0900

    이메일 인증이 가능하도록 하라

--- app-server/app/src/test/java/com/codesoom/myseat/services/verification/email/EmailVerificationRequestServiceTest.java
+++ app-server/app/src/test/java/com/codesoom/myseat/services/verification/email/EmailVerificationRequestServiceTest.java
@@ -1,7 +1,10 @@
 package com.codesoom.myseat.services.verification.email;
 
 import com.codesoom.myseat.domain.EmailToken;
+import com.codesoom.myseat.domain.User;
+import com.codesoom.myseat.helpers.EmailHelper;
 import com.codesoom.myseat.repositories.EmailTokenRepository;
+import com.codesoom.myseat.repositories.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,15 +27,31 @@ class EmailVerificationRequestServiceTest {
     private EmailVerificationRequestService service;
 
     @Mock
-    private EmailTokenRepository repository;
+    private EmailTokenRepository emailTokenRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private EmailHelper helper;
 
     @DisplayName("주어진 유저 id로 생성된 토큰은 유효하다.")
     @Test
-    void requestEmailVerificationTest() {
+    void requestEmailVerificationTest() throws IOException {
         //given
         Long userId = 1L;
-        given(repository.save(any()))
+
+        User mockUser = User.builder()
+                .id(userId)
+                .name("김철수")
+                .email("soo@email.com")
+                .password("$2a$10$hxqWrlGa7SQcCEGURjmuQup4J9kN6qnfr4n7j7R3LvzHEoEOUTWeW")
+                .build();
+
+        given(emailTokenRepository.save(any()))
                 .willReturn(EmailToken.createEmailToken(userId));
+        given(userRepository.findById(any()))
+                .willReturn(Optional.of(mockUser));
 
         //when
         EmailToken token = service.requestEmailVerification(userId);
