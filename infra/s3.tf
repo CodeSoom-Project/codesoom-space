7ca55cfe7b32cdd1aaa517459858515ee1e47725
hannut91 <hannut91@gmail.com>
2022-10-28 18:36:51 +0900

    관리자 페이지를 배포하라

--- infra/s3.tf
+++ infra/s3.tf
@@ -10,3 +10,16 @@ resource "aws_s3_bucket" "bucket" {
 
   tags = local.tags
 }
+
+resource "aws_s3_bucket" "admin-bucket" {
+  bucket = "space-admin.codesoom.com"
+
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  tags = local.tags
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,6 @@
 locals {
   acm = "arn:aws:acm:us-east-1:466782999783:certificate/be52637a-b8da-43e5-bce8-99358955b673"
+  admin-acm = "arn:aws:acm:us-east-1:466782999783:certificate/87a5bd62-d603-49c6-a61f-2c29f867b98a"
 
   tags = {
     env  = "production",
