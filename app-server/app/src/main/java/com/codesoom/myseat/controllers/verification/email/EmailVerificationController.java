4bada4897f4cea81d0e513ef194a11ea360a5f07
HanJiyoung <jiyounghaan@gmail.com>
2022-10-29 20:55:45 +0900

    이메일 인증이 잘 오는지 확인할 수 있도록 로그를 찍어라

--- app-server/app/src/main/java/com/codesoom/myseat/controllers/verification/email/EmailVerificationController.java
+++ app-server/app/src/main/java/com/codesoom/myseat/controllers/verification/email/EmailVerificationController.java
@@ -3,6 +3,7 @@ package com.codesoom.myseat.controllers.verification.email;
 import com.codesoom.myseat.dto.EmailVerificationResponse;
 import com.codesoom.myseat.security.UserAuthentication;
 import com.codesoom.myseat.services.verification.email.EmailVerificationRequestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /** 이메일 인증 요청 컨트롤러 */
+@Slf4j
 @CrossOrigin
 @RequestMapping("/verification/email")
 @RestController
@@ -28,6 +30,7 @@ public class EmailVerificationController {
     @PostMapping
     public EmailVerificationResponse requestEmailVerification(
             @AuthenticationPrincipal UserAuthentication principal) {
+        log.info("이메일 토큰 인증");
         return new EmailVerificationResponse(
                 service.requestEmailVerification(principal.getId()));
     }
