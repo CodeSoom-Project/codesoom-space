ca45987b6806c8a1631890d54855ab0b83046e7a
Woo Dy <kjop7419@naver.com>
2022-10-29 15:23:09 +0900

    로그인이 실패해도 예약 내역 페이지로 이동하는 이슈를 수정하라
    errorMessage를 감지하여 에러가 발생하면 예약 내역 페이지로 이동하지 않게끔
    리팩토링하는 작업을 진행했습니다.

--- app-admin/src/Containers/LoginFormContainer/index.tsx
+++ app-admin/src/Containers/LoginFormContainer/index.tsx
@@ -1,32 +1,52 @@
-import LoginForm from '../../components/LoginForm';
+import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
+
+import { useAppDispatch } from '../../hooks';
+
+import { RootState } from '../../store';
 
 import {
   requestLogin,
   changeLoginField,
+  setErrorMessage,
 } from '../../redux/authSlice';
 
-
-import { RootState } from '../../store';
-import { useAppDispatch } from '../../hooks';
-import { useNavigate } from 'react-router-dom';
+import LoginForm from '../../components/LoginForm';
 
 const LoginFormContainer = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const { loginFields: { email, password } } = useSelector((state: RootState) => state.auth);
+  const {
+    loginFields: { email, password },
+    errorMessage,
+    accessToken,
+  } = useSelector((state: RootState) => state.auth);
 
   const onChange = ({ name, value }: { name: string, value: string }) => {
     dispatch(changeLoginField({ name, value }));
   };
 
-  const handleSubmit: React.MouseEventHandler<HTMLButtonElement> = async () => {
-    await dispatch(requestLogin());
-
-    navigate('reservations');
+  const handleSubmit: React.MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(requestLogin());
   };
+
+  useEffect(() => {
+    if (errorMessage) {
+      alert(errorMessage);
+    }
+
+    if (accessToken) {
+      navigate('reservations');
+    }
+
+    return () => {
+      dispatch(setErrorMessage(''));
+    };
+  }, [errorMessage, accessToken]);
 
   return (
     <LoginForm
